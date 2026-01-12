698de0cc839cff1298616d68b330c33a57c097f6
eb613819 <eb613819@ohio.edu>
2026-01-12 03:50:43 -0500

    Add basic routing and navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ShopComponent } from './pages/shop/shop.component';
+import { ProductComponent } from './pages/product/product.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { AboutComponent } from './pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'shop', component: ShopComponent },
+    { path: 'product/:slug', component: ProductComponent },
+    { path: 'cart', component: CartComponent },
+    { path: 'about', component: AboutComponent },
+    { path: '**', redirectTo: '' },
+];
